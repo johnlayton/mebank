64656871378b0eed31cff5ac30166dd31b8e0e41
john layton <johnstewartlayton@gmail.com>
2015-08-19 20:03:39 +1000

    test for parsing the date from the transaction string

--- src/main/java/com/touchcorp/Transaction.java
+++ src/main/java/com/touchcorp/Transaction.java
@@ -1,22 +1,35 @@
 package com.touchcorp;
 
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+
 public final class Transaction
 {
   private final String hash;
+  private final long day;
 
-  public Transaction( final String hash )
+  public Transaction( final String hash,
+                      final long day )
   {
     this.hash = hash;
+    this.day = day;
   }
 
   public static Transaction parse( final String input )
   {
     final String[] elements = input.split( "," );
-    return new Transaction( elements[0] );
+    final String hash = elements[ 0 ];
+    final long day = DateTimeFormatter.ISO_LOCAL_DATE_TIME.parse( elements[ 1 ] ).getLong( ChronoField.EPOCH_DAY );
+    return new Transaction( hash, day );
   }
 
   public String getHash()
   {
     return hash;
+  }
+
+  public long getDay()
+  {
+    return day;
   }
 }
--- src/test/java/com/touchcorp/TransactionTest.java
+++ src/test/java/com/touchcorp/TransactionTest.java
@@ -1,14 +1,15 @@
 package com.touchcorp;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TransactionTest
 {
   @Test
   public void shouldParseLogEntry()
   {
-    final Transaction transaction = Transaction.parse( "10d7ce2f43e35fa57d1bbf8b1e2, 2014-04-29T13:15:54, 10.00" );
+    final Transaction transaction = Transaction.parse( "10d7ce2f43e35fa57d1bbf8b1e2,2014-04-29T13:15:54,10.00" );
     assertEquals( "10d7ce2f43e35fa57d1bbf8b1e2", transaction.getHash() );
+    assertEquals( 16189, transaction.getDay() );
   }
 }
